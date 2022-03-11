53ab6353807d91a7698b55920998222e693c4ee9
OS! <vinhhoangngoc22@gmail.com>
2022-03-11 18:10:58 +0700

    update

---
+++ src/demo/ConsumerFunctional.java
@@ -0,0 +1,29 @@
+package demo;
+
+import java.util.function.Consumer;
+
+public class ConsumerFunctional {
+	public static void main(String[] args) {
+		Customer man = new Customer("Jack", 14);
+		greetCustomerConsumer.accept(man);
+	}
+
+	static Consumer<Customer> greetCustomerConsumer = customer -> System.out
+			.println("Hello: " + customer.nameCustomer + " Your Age: " + customer.ageCustomer);
+
+	static class Customer {
+		private String nameCustomer;
+		private int ageCustomer;
+
+		public Customer(String nameCustomer, int ageCustomer) {
+			super();
+			this.nameCustomer = nameCustomer;
+			this.ageCustomer = ageCustomer;
+		}
+
+		public Customer() {
+			super();
+		}
+
+	}
+}
